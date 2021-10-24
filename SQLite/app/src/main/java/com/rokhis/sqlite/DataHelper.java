7338c48ceb9375d42d8e655d819e0e8e443e3fef
M Rokhis <75311518+rokhis@users.noreply.github.com>
2021-10-24 20:30:48 +0700

    Add files via upload

---
+++ SQLite/app/src/main/java/com/rokhis/sqlite/DataHelper.java
@@ -0,0 +1,29 @@
+package com.rokhis.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataHelper extends  SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "biodata.db";
+    private static final int DATABASE_VERSION = 1;
+    public DataHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate (SQLiteDatabase Db){
+        String sql = "create table biodata(no integer primary key, nama text null, tgl date, jk text null, alamat text null);";
+        Log.d("Data", "onCreate: " + sql);
+        Db.execSQL(sql);
+        sql = "INSERT INTO biodata (no, nama, tgl, jk, alamat) VALUES ('21', 'M Rokhis', '03-08-2001', 'L', 'Banyuwangi');";
+        Db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2){
+
+    }
+}
